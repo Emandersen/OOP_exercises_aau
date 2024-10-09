517c7a1fbd51a1fd7847c968f0b36b29b89a93ac
Lenovo <minnyecomputer.com@gmail.com>
2024-10-09 12:54:06 +0200

    Added session files

---
+++ OOP_E24/src/session4/B_Multiple_Inheritance/Exercise2.java
@@ -0,0 +1,36 @@
+package session4.B_Multiple_Inheritance;
+
+/**
+ * Exercise:
+ *  - Create a class called ElectricPetrolStation that provides a method chargeVehicle(ElectricVehicle vehicle). This method should:
+ *      - Accept only instances of vehicles that implement the ElectricVehicle interface.
+ *      - Call the batteryLevel() method to check the current battery level before charging.
+ *      - Simulate charging the vehicle by increasing the battery level (for simplicity, assume charging adds 10% to the battery, but does not exceed 100%).
+ *
+ *  - Modify the ElectricVehicle interface to allow updating the battery level of a vehicle.
+ *
+ *  - Create a class to represent Electric Buses manufactured by Volvo.
+ *
+ *  - Add code below to simulate a scenario where both a TeslaS and a Volvo Electric Bus go to an ElectricPetrolStation and get charged.
+ *
+ */
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+        /*
+        //Did you have time to finish the exercise?
+        boolean I_did_finish = true;
+
+        //Did you understand the concepts used in the exercise?
+        boolean I_did_understand = true;
+
+        //Any comments
+        String comment = "No comments";
+
+        feedback.Feedback.submit("session4_B2", I_did_finish, I_did_understand, comment);
+        */
+
+    }
+}
