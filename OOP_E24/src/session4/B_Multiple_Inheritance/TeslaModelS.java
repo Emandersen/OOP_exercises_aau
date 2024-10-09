f867528343a7b3918ba407a45212eccf1a5360a1
Lenovo <minnyecomputer.com@gmail.com>
2024-10-09 14:11:00 +0200

    edited session files

---
+++ OOP_E24/src/session4/B_Multiple_Inheritance/TeslaModelS.java
@@ -0,0 +1,22 @@
+package session4.B_Multiple_Inheritance;
+
+public class TeslaModelS extends SportsCar implements ElectricVehicle {
+    private double battery = 100.0;
+
+    public int speed() {
+        return 0;
+    }
+
+    public boolean goFast() {
+        System.out.println("Going fast");
+        return false;
+    }
+
+    public double batteryLevel() {
+        return battery;
+    }
+
+    public void charge() {
+        this.battery += 10;
+    }
+}
